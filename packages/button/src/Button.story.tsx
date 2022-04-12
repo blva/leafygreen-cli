7e5e5eff8a673b292b96cbc0d19fa3becbb0a46a
Adam Thompson <adam.thompson@mongodb.com>
2022-04-12 18:55:35 -0400

    Refactors Button story

--- packages/button/src/Button.story.tsx
+++ packages/button/src/Button.story.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { storiesOf } from '@storybook/react';
 import { boolean, select, text } from '@storybook/addon-knobs';
 import { uiColors } from '@leafygreen-ui/palette';
 import { css, cx } from '@leafygreen-ui/emotion';
@@ -7,97 +6,148 @@ import Icon, { glyphs } from '@leafygreen-ui/icon';
 import LeafygreenProvider from '@leafygreen-ui/leafygreen-provider';
 import Button, { Variant, Size } from '.';
 
-storiesOf('Button', module)
-  .add('Default', () => {
-    const buttonText = text('Text', 'MongoDB');
-    const variant = select('Variant', Object.values(Variant), Variant.Default);
-    const size = select('Size', Object.values(Size), Size.Default);
-    const baseFontSize = select('Base Font Size', [14, 16], 14);
-    const disabled = boolean('Disabled', false);
-    const darkMode = boolean('Dark Mode', false);
-    const href = select(
-      'href',
-      [undefined, 'https://mongodb.design'],
-      undefined,
-    );
-    const leftGlyph = select(
-      'Left Glyph',
-      [...Object.keys(glyphs), undefined],
-      undefined,
-    );
-    const rightGlyph = select(
-      'Right Glyph',
-      [...Object.keys(glyphs), undefined],
-      undefined,
-    );
+export default {
+  title: 'Button',
+  component: Button,
+};
 
-    const wrapperStyle = cx(
-      css`
-        background-color: ${!darkMode ? uiColors.white : uiColors.gray.dark3};
-        padding: 40px;
-      `,
-    );
+export const Default = () => {
+  const buttonText = text('Text', 'MongoDB');
+  const variant = select('Variant', Object.values(Variant), Variant.Default);
+  const size = select('Size', Object.values(Size), Size.Default);
+  const baseFontSize = select('Base Font Size', [14, 16], 14);
+  const disabled = boolean('Disabled', false);
+  const darkMode = boolean('Dark Mode', false);
+  const href = select('href', [undefined, 'https://mongodb.design'], undefined);
+  const leftGlyph = select(
+    'Left Glyph',
+    [...Object.keys(glyphs), undefined],
+    undefined,
+  );
+  const rightGlyph = select(
+    'Right Glyph',
+    [...Object.keys(glyphs), undefined],
+    undefined,
+  );
 
-    return (
-      <LeafygreenProvider>
-        <div className={wrapperStyle}>
-          <Button
-            variant={variant}
-            darkMode={darkMode}
-            size={size}
-            disabled={disabled}
-            baseFontSize={baseFontSize}
-            href={href}
-            leftGlyph={leftGlyph ? <Icon glyph={leftGlyph} /> : undefined}
-            rightGlyph={rightGlyph ? <Icon glyph={rightGlyph} /> : undefined}
-          >
-            {buttonText}
-          </Button>
-        </div>
-      </LeafygreenProvider>
-    );
-  })
-  .add('Icon only', () => {
-    const variant = select('Variant', Object.values(Variant), Variant.Default);
-    const size = select('Size', Object.values(Size), Size.Default);
-    const baseFontSize = select('Base Font Size', [14, 16], 14);
-    const disabled = boolean('Disabled', false);
-    const darkMode = boolean('Dark Mode', false);
-    const href = select(
-      'href',
-      [undefined, 'https://mongodb.design'],
-      undefined,
-    );
-    const leftGlyph = select(
-      'Left Glyph',
-      [...Object.keys(glyphs), undefined],
-      'Beaker',
-    );
-    const rightGlyph = select(
-      'Right Glyph',
-      [...Object.keys(glyphs), undefined],
-      undefined,
-    );
-
-    const className = css`
+  const wrapperStyle = cx(
+    css`
       background-color: ${!darkMode ? uiColors.white : uiColors.gray.dark3};
       padding: 40px;
-    `;
+    `,
+  );
 
-    return (
-      <LeafygreenProvider>
-        <div className={className}>
-          <Button
-            variant={variant}
-            darkMode={darkMode}
-            size={size}
-            disabled={disabled}
-            baseFontSize={baseFontSize}
-            href={href}
-            leftGlyph={leftGlyph ? <Icon glyph={leftGlyph} /> : undefined}
-            rightGlyph={rightGlyph ? <Icon glyph={rightGlyph} /> : undefined}
-          />
-        </div>
-      </LeafygreenProvider>
-    );
-  });
+  return (
+    <LeafygreenProvider>
+      <div className={wrapperStyle}>
+        <Button
+          variant={variant}
+          darkMode={darkMode}
+          size={size}
+          disabled={disabled}
+          baseFontSize={baseFontSize}
+          href={href}
+          leftGlyph={leftGlyph ? <Icon glyph={leftGlyph} /> : undefined}
+          rightGlyph={rightGlyph ? <Icon glyph={rightGlyph} /> : undefined}
+        >
+          {buttonText}
+        </Button>
+      </div>
+    </LeafygreenProvider>
+  );
+};
+
+// storiesOf('Button', module)
+//   .add('Default', () => {
+//     const buttonText = text('Text', 'MongoDB');
+//     const variant = select('Variant', Object.values(Variant), Variant.Default);
+//     const size = select('Size', Object.values(Size), Size.Default);
+//     const baseFontSize = select('Base Font Size', [14, 16], 14);
+//     const disabled = boolean('Disabled', false);
+//     const darkMode = boolean('Dark Mode', false);
+//     const href = select(
+//       'href',
+//       [undefined, 'https://mongodb.design'],
+//       undefined,
+//     );
+//     const leftGlyph = select(
+//       'Left Glyph',
+//       [...Object.keys(glyphs), undefined],
+//       undefined,
+//     );
+//     const rightGlyph = select(
+//       'Right Glyph',
+//       [...Object.keys(glyphs), undefined],
+//       undefined,
+//     );
+
+//     const wrapperStyle = cx(
+//       css`
+//         background-color: ${!darkMode ? uiColors.white : uiColors.gray.dark3};
+//         padding: 40px;
+//       `,
+//     );
+
+//     return (
+//       <LeafygreenProvider>
+//         <div className={wrapperStyle}>
+//           <Button
+//             variant={variant}
+//             darkMode={darkMode}
+//             size={size}
+//             disabled={disabled}
+//             baseFontSize={baseFontSize}
+//             href={href}
+//             leftGlyph={leftGlyph ? <Icon glyph={leftGlyph} /> : undefined}
+//             rightGlyph={rightGlyph ? <Icon glyph={rightGlyph} /> : undefined}
+//           >
+//             {buttonText}
+//           </Button>
+//         </div>
+//       </LeafygreenProvider>
+//     );
+//   })
+//   .add('Icon only', () => {
+//     const variant = select('Variant', Object.values(Variant), Variant.Default);
+//     const size = select('Size', Object.values(Size), Size.Default);
+//     const baseFontSize = select('Base Font Size', [14, 16], 14);
+//     const disabled = boolean('Disabled', false);
+//     const darkMode = boolean('Dark Mode', false);
+//     const href = select(
+//       'href',
+//       [undefined, 'https://mongodb.design'],
+//       undefined,
+//     );
+//     const leftGlyph = select(
+//       'Left Glyph',
+//       [...Object.keys(glyphs), undefined],
+//       'Beaker',
+//     );
+//     const rightGlyph = select(
+//       'Right Glyph',
+//       [...Object.keys(glyphs), undefined],
+//       undefined,
+//     );
+
+//     const className = css`
+//       background-color: ${!darkMode ? uiColors.white : uiColors.gray.dark3};
+//       padding: 40px;
+//     `;
+
+//     return (
+//       <LeafygreenProvider>
+//         <div className={className}>
+//           <Button
+//             variant={variant}
+//             darkMode={darkMode}
+//             size={size}
+//             disabled={disabled}
+//             baseFontSize={baseFontSize}
+//             href={href}
+//             leftGlyph={leftGlyph ? <Icon glyph={leftGlyph} /> : undefined}
+//             rightGlyph={rightGlyph ? <Icon glyph={rightGlyph} /> : undefined}
+//           />
+//         </div>
+//       </LeafygreenProvider>
+//     );
+//   });
--- packages/button/src/types.ts
+++ packages/button/src/types.ts
@@ -28,18 +28,72 @@ type Mode = typeof Mode[keyof typeof Mode];
 type FontSize = 14 | 16;
 
 interface ButtonProps {
+  /**
+   * Sets the style variant of the button.
+   * @default `'default'`
+   */
   variant?: Variant;
+  /**
+   * Determines if the component renders in dark mode
+   * @default `false`
+   */
   darkMode?: boolean;
-  baseFontSize?: FontSize;
+  /**
+   * Sets the size variant of the button.
+   * @default `'default'`
+   */
   size?: Size;
-  disabled?: boolean;
-  className?: string;
+
+  /**
+   * The content that will appear inside of the `<Button />` component.
+   * @default `null`
+   */
   children?: React.ReactNode;
-  leftGlyph?: React.ReactElement;
-  rightGlyph?: React.ReactElement;
-  href?: string;
-  type?: JSX.IntrinsicElements['button']['type'];
+
+  /**
+   * Adds a className to the class attribute.
+   * @default `''`
+   */
+  className?: string;
+
+  /**
+   * Disables the button
+   * @default `false`
+   */
+  disabled?: boolean;
+
+  /**
+   * Determines the root element. An `a` tags can be supplied to replace `button` from being the DOM element that wraps the component.
+   * @default `button`
+   */
   as?: keyof JSX.IntrinsicElements;
+
+  /**
+   * If a href is supplied it will change the `as` value, such that the component renders inside of an `a` tag instead of inside of a `button` tag.
+   * @default
+   */
+  href?: string;
+
+  /**
+   * Glyph that will appear to the left of text, if there is text provided via the children prop. If no children are supplied to the component, passing an Icon here will render the button as an icon-only button.
+   * @default
+   */
+  leftGlyph?: React.ReactElement;
+  /**
+   * Glyph that will appear to the right of text, if there is text provided via the children prop. If no children are supplied to the component, passing an Icon here will render the button as an icon-only button.
+   * @default
+   */
+  rightGlyph?: React.ReactElement;
+  /**
+   * Determines the base font-size of the component
+   * @default `14`
+   */
+  baseFontSize?: FontSize;
+
+  /**
+   * The native HTML `type` attribute on a Button
+   */
+  type?: JSX.IntrinsicElements['button']['type'];
 }
 
 export { Variant, Size, Mode, FontSize, ButtonProps };
